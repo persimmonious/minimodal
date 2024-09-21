9b59eb8d32933f41b5ef3721b7cb6e8deb9f20d9
msharipov <msharipovr@gmail.com>
2024-09-20 21:36:51 -0400

    feat: parse file name from command line

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 mod app;
-use std::io;
 
-fn main() -> io::Result<()> {
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let config = app::parse_command_line()?;
     let mut terminal = ratatui::init();
     terminal.clear()?;
-    let app_result = app::run(&mut terminal);
+    let app_result = app::run(&mut terminal, config);
     ratatui::restore();
-    app_result
+    match app_result {
+        Err(error) => Err(Box::new(error)),
+        Ok(()) => Ok(())
+    }
 }
