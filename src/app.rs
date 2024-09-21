0a232deb2db9056340a0eba1bcab2964eaf740d0
msharipov <msharipovr@gmail.com>
2024-09-21 01:37:58 -0400

    feat: initialize buffers from file names

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,38 @@
+mod buffer;
 use crate::config::Config;
+use buffer::Buffer;
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind},
     style::Stylize,
     widgets::Paragraph,
     DefaultTerminal,
 };
-use std::io;
+use std::{fs, io};
 
-mod buffer;
+pub fn initialize_buffers(config: &Config) -> Result<Vec<Buffer>, io::Error> {
+    if config.file_names.is_empty() {
+        return Ok(vec![Buffer::untitled()]);
+    }
+    let mut buffers: Vec<Buffer> = vec![];
+    for name in &config.file_names {
+        if fs::exists(name)? {
+            buffers.push(Buffer::load(name, name)?);
+        } else {
+            buffers.push(Buffer::empty(name, name));
+        }
+    }
+    Ok(buffers)
+}
 
 pub fn run(terminal: &mut DefaultTerminal, config: Config) -> io::Result<()> {
-    let sampletext = match config.file_names.len() {
+    let buffers = initialize_buffers(&config)?;
+    let sampletext = match buffers.len() {
         0 => format!("No files loaded.\nPress 'q' to quit."),
         _ => format!(
             "{}Press 'q' to quit.",
-            config
-                .file_names
+            buffers
                 .iter()
+                .map(|buf| buf.read_name().map_or("Untitled", |x| x))
                 .map(|name| format!("File: {name}\n"))
                 .fold(String::new(), |mut acc, x| {
                     acc.push_str(&x);
--- src/app/buffer.rs
+++ src/app/buffer.rs
@@ -1,6 +1,41 @@
+use std::{fs, io};
+
 pub struct Buffer {
-    name: String,
-    path: String,
+    name: Option<String>,
+    path: Option<String>,
     pub lines: Vec<String>,
 }
 
+impl Buffer {
+    pub fn read_name<'a>(&'a self) -> Option<&'a str> {
+        match &self.name {
+            Some(name) => Some(&name),
+            None => None,
+        }
+    }
+
+    pub fn untitled() -> Self {
+        return Buffer {
+            name: None,
+            path: None,
+            lines: vec![],
+        };
+    }
+
+    pub fn load(name: &str, path: &str) -> io::Result<Self> {
+        let contents = fs::read_to_string(path)?;
+        Ok(Buffer {
+            name: Some(name.to_owned()),
+            path: Some(path.to_owned()),
+            lines: contents.lines().map(|line| line.to_owned()).collect(),
+        })
+    }
+
+    pub fn empty(name: &str, path: &str) -> Self {
+        Buffer {
+            name: Some(name.to_owned()),
+            path: Some(path.to_owned()),
+            lines: vec![],
+        }
+    }
+}
